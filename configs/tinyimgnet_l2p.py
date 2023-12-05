8c7714986c10bc36ad6018b4ebc4037301c0ea41
adghin <adrian.ghinea00@gmail.com>
2023-12-05 17:33:47 +0100

    Update tinyimgnet_l2p.py

--- configs/tinyimgnet_l2p.py
+++ configs/tinyimgnet_l2p.py
@@ -100,4 +100,4 @@ def get_args_parser(subparsers):
     subparsers.add_argument('--print_freq', type=int, default=10, help = 'The frequency of printing')
 
     #Wandb
-    subparser.add_argument('--nowand',type=int, default=1, help='Log to wandb')
+    subparsers.add_argument('--nowand',type=int, default=1, help='Log to wandb')
