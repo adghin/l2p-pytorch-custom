fb0477b81a6dce91156ca7d9d7b6b0c756081a89
adghin <adrian.ghinea00@gmail.com>
2023-12-08 10:42:19 +0100

    Update vision_transformer.py

--- vision_transformer.py
+++ vision_transformer.py
@@ -330,7 +330,7 @@ class VisionTransformer(nn.Module):
     """
 
     def __init__(
-            self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, global_pool='token',
+            self, img_size=224, patch_size=16, in_chans=3, num_classes=21843, global_pool='token',
             embed_dim=768, depth=12, num_heads=12, mlp_ratio=4., qkv_bias=True, init_values=None,
             class_token=True, no_embed_class=False, fc_norm=None, drop_rate=0., attn_drop_rate=0., drop_path_rate=0.,
             weight_init='', embed_layer=PatchEmbed, norm_layer=None, act_layer=None, block_fn=Block,
