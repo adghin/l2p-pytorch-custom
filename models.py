fead9e5ca5d7d6ee8029bf3763df63b44e09d81b
adghin <adrian.ghinea00@gmail.com>
2023-12-08 09:49:44 +0100

    Update models.py

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ from timm.models.registry import register_model
 from vision_transformer import _create_vision_transformer
 
 __all__ = [
-    'vit_tiny_patch16_224', 'vit_small_patch16_224', 'vit_base_patch16_224',
+    'vit_tiny_patch16_224', 'vit_small_patch16_224', 'vit_base_patch16_224', 'vit_base_patch16_224_in21k'
 ]
 
 @register_model
