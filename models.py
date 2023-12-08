7575123a5b4afe92b185eaff807486421a5e5d8f
adghin <adrian.ghinea00@gmail.com>
2023-12-08 09:50:38 +0100

    Update models.py

--- models.py
+++ models.py
@@ -43,7 +43,9 @@ def vit_base_patch16_224(pretrained=False, **kwargs):
 
 @register_model
 def vit_base_patch16_224_in21k(pretrained=False, **kwargs):
-    """ViT-Base (ViT-B/16) pretrained on ImageNet-21k
+    """ ViT-Base model (ViT-B/16) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-21k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    NOTE: this model has valid 21k classifier head and no representation (pre-logits) layer
     """
     model_kwargs = dict(patch_size=16, embed_dim=768, depth=12, num_heads=12, **kwargs)
     model = _create_vision_transformer('vit_base_patch16_224_in21k', pretrained=pretrained, **model_kwargs)
