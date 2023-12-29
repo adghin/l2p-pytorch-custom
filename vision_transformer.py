de06992a95bbbaec6ca26517ad56585175b0a602
adghin <adrian.ghinea00@gmail.com>
2023-12-29 16:58:29 +0100

    Update vision_transformer.py

--- vision_transformer.py
+++ vision_transformer.py
@@ -95,7 +95,8 @@ default_cfgs = {
         #url='https://storage.googleapis.com/vit_models/augreg/B_16-i21k-300ep-lr_0.001-aug_medium1-wd_0.1-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.01-res_224.npz'),
         #url='https://storage.googleapis.com/vit_models/augreg/B_16-i1k-300ep-lr_0.001-aug_medium1-wd_0.03-do_0.0-sd_0.0.npz'),
         #url='https://storage.googleapis.com/vit_models/imagenet21k%2Bimagenet2012/ViT-B_16.npz'),
-        url='https://storage.googleapis.com/vit_models/augreg/B_16-i21k-300ep-lr_0.001-aug_medium1-wd_0.1-do_0.0-sd_0.0.npz'),    
+        #url='https://storage.googleapis.com/vit_models/augreg/B_16-i21k-300ep-lr_0.001-aug_medium1-wd_0.1-do_0.0-sd_0.0.npz'),    
+        hf_hub_id='timm/'),
     'vit_base_patch16_384': _cfg(
         url='https://storage.googleapis.com/vit_models/augreg/'
             'B_16-i21k-300ep-lr_0.001-aug_medium1-wd_0.1-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.01-res_384.npz',
